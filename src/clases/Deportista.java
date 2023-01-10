c65bcad143bf7acb18798d3850ee4b6f5e569a84
Irene Herrero <Irene Herrero@HZ114831.H012108.NET>
2023-01-10 13:34:02 +0100

    Primer comit

--- src/clases/Deportista.java
+++ src/clases/Deportista.java
@@ -1,5 +1,5 @@
 package clases;
 
-public abstract class Deportista {
+public class Deportista {
 
 }
--- src/clases/Jugador.java
+++ src/clases/Jugador.java
@@ -1,5 +1,5 @@
 package clases;
 
-public class Jugador extends Deportista{
+public class Jugador {
 
 }
